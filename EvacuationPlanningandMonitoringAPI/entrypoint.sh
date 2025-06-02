02d4e107445a2872352c81f5c981d084aa59e9ae
ILJIBEARZ\thata <thatawat.project@gmail.com>
2025-06-02 18:26:22 +0700

    update

--- EvacuationPlanningandMonitoringAPI/entrypoint.sh
+++ EvacuationPlanningandMonitoringAPI/entrypoint.sh
@@ -1,9 +1,10 @@
-#!binbash
+#!/bin/bash
 
 # แสดงข้อความใน log
 echo [+] Starting WireGuard VPN...
+
 # เปิด VPN จาก config ไฟล์ wg0.conf
-wg-quick up etcwireguardwg0.conf
+wg-quick up /etc/wireguard/wg0.conf
 
 # รอ VPN เสถียรก่อนเรียก Web API
 echo [+] Waiting for VPN to be ready...
@@ -12,4 +13,4 @@ ping -c 2 10.0.0.1
 
 # รัน Web API
 echo [+] Starting ASP.NET Core Web API...
-dotnet EvacuationPlanningandMonitoringAPI.dll
+exec dotnet EvacuationPlanningandMonitoringAPI.dll
